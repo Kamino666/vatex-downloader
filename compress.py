781fa451ef425906c95fd2337d6d3aa0f7e83b01
Kamino666 <516015417@qq.com>
2022-10-09 11:56:50 +0800

    compress video script

---
+++ compress.py
@@ -0,0 +1,22 @@
+import subprocess
+from pathlib import Path
+from tqdm import tqdm
+
+
+"""
+ffmpeg -i input.mp4 -vf scale=320:240 -y output.mp4
+"""
+OUTPUT_DIR = Path("validation_low_res")
+INPUT_DIR = Path("processed_videos")
+
+OUTPUT_DIR.mkdir(exist_ok=True)
+for vid in tqdm(list(INPUT_DIR.glob("*.mp4"))):
+    result = subprocess.run([
+        "ffmpeg",
+        "-i", str(vid),
+        "-vf", "scale=320:240",
+        "-y", str(OUTPUT_DIR / vid.name)
+    ], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    if result.returncode != 0:
+        print(f"vid: {vid.stem} code: {result.returncode}")
+
